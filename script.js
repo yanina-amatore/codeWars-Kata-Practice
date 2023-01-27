9b7d8cdddf871b35f48353a7f6d9991c920dc045
Yanina Amatore <yaninaamatore@gmail.com>
2023-01-27 15:01:35 +1300

    Sum of the first nth term

--- script.js
+++ script.js
@@ -18,14 +18,36 @@ let arr2 = [160, 3, 1719, 19, 11, 13, -21]
 // Should return: 160 (the only even number)
 let newarr = [22, 4, 3, 6]
 
-
 function findOutlier(arr) {
-  let isEvenarr = arr.filter (n => n % 2 == 0)
-  let isOddarr = arr.filter (n => n % 2 !== 0)
-  
+  let isEvenarr = arr.filter((n) => n % 2 == 0)
+  let isOddarr = arr.filter((n) => n % 2 !== 0)
+
   return isEvenarr.length == 1 ? isEvenarr[0] : isOddarr[0]
 }
 
-console.log(findOutlier(arr))
-console.log(findOutlier(arr2))
-console.log(findOutlier(newarr))
+// console.log(findOutlier(arr))
+// console.log(findOutlier(arr2))
+// console.log(findOutlier(newarr))
+
+// ---- Sum of the first nth term of Series
+
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+function seriesSum(n) {
+  let sum = 0
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + i * 3)
+  }
+  return n === 0 ? '0.00' : parseFloat(sum).toFixed(2).toString()
+}
+// console.log(seriesSum(1)); // 1.00
+seriesSum(1)
+console.log(typeof seriesSum(1), seriesSum(1)) // 1.25
+console.log(seriesSum(5)) // 1.57
